bf76417304825db4f45105f50f2746cbc37c8709
Ivan Zhukov <zhkvivan@gmail.com>
2022-02-26 18:24:43 +0000

    reviews section is done

--- src/Components/Reviews/Reviews.js
+++ src/Components/Reviews/Reviews.js
@@ -1,44 +1,88 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './Reviews.module.scss';
+import portrait1 from '../../pics/portrait1.png';
+import portrait2 from '../../pics/portrait2.png';
+import portrait3 from '../../pics/portrait3.png';
+import portrait4 from '../../pics/portrait4.png';
+import portrait5 from '../../pics/portrait5.png';
 
 export const Reviews = () => {
 	const reviewsArr = [
 		{
-			// photo:
+			id: 0,
+			photo: portrait1,
 			name: 'Елена',
-			review:
+			reviewText:
 				'Нужно было провести диагностику зубов, Искала долго подходящую клинику. В этой сделали все очень качественно и недрого, что немаловажно. Приду еще раз.',
 			selected: false,
 		},
 		{
-			// photo:
+			id: 1,
+			photo: portrait2,
 			name: 'Игорь',
-			review:
-				'Нужно было провести диагностику зубов, Искала долго подходящую клинику. В этой сделали все очень качественно и недрого, что немаловажно. Приду еще раз.',
+			reviewText:
+				'В принципе неплохая клиника. Цены тоже ниче так. Но как по мне, персонал мог быть и получше)',
 			selected: true,
 		},
 		{
-			// photo:
+			id: 2,
+			photo: portrait3,
 			name: 'Татьяна',
-			review:
-				'Нужно было провести диагностику зубов, Искала долго подходящую клинику. В этой сделали все очень качественно и недрого, что немаловажно. Приду еще раз.',
+			reviewText:
+				'Мне всё всегда нравится. Особенно нравится скидка 200 рублей которую мне дали за хороший отзыв! Приду еще!',
+			selected: false,
+		},
+		{
+			id: 3,
+			photo: portrait4,
+			name: 'Олежек',
+			reviewText: 'Хорошая шиномонтажка! Ой, не туда.',
 			selected: false,
 		},
 	];
+
+	const [currentReview, setCurrentReview] = useState(reviewsArr[1]);
 	return (
-		<div className={styles.wrap}>
+		<section className={styles.wrap}>
 			<div className={styles.container}>
 				<div className={styles.content}>
-					{reviewsArr.map((review) => {
-						return (
-							<div className={styles.review_wrap}>
-								<div className={styles.photo}></div>
-								<div className={styles.name}>{review.name}</div>
-							</div>
-						);
-					})}
+					<div className={styles.photos_wrap}>
+						{reviewsArr.map((review) => {
+							return (
+								<div
+									className={
+										currentReview.id === review.id
+											? styles.photo_wrap_selected
+											: styles.photo_wrap
+									}
+									onClick={(e) => {
+										setCurrentReview(reviewsArr[review.id]);
+									}}
+								>
+									<img src={review.photo} alt="" className={styles.photo} />
+									<div className={styles.photo_bg}></div>
+								</div>
+							);
+						})}
+					</div>
+
+					<Slide
+						name={currentReview.name}
+						reviewText={currentReview.reviewText}
+					/>
 				</div>
 			</div>
-		</div>
+		</section>
+	);
+};
+
+const Slide = ({ photo, name, reviewText }) => {
+	return (
+		<>
+			<div className={styles.review_wrap}>
+				<div className={styles.name}>{name}</div>
+				<div className={styles.text}>{reviewText}</div>
+			</div>
+		</>
 	);
 };
